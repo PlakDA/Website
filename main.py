b711d89ca40c129fae218e6659b6dc801bab60e4
Iaroslav Plaksin <Plak.I.A@yandex.ru>
2024-03-28 21:37:12 +0300

    commit

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, jsonify, render_template
+from flask_restful import Api
 
+from api import menu as menu_api
+from api import orders as order_api
+from api import users as users_api
+from data import db_session
 
 app = Flask(__name__)
+api = Api(app)
+app.config['SECRET_KEY'] = 'velcake_secret_key'
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 @app.route('/')
@@ -19,5 +36,16 @@ def create():
     return render_template('create.html')
 
 
+def api_connect():
+    api.add_resource(menu_api.MenuResource, '/api/menu/<int:dish_id>')
+    api.add_resource(menu_api.MenuListResource, '/api/menu')
+    api.add_resource(order_api.OrderResource, '/api/orders/<int:order_id>')
+    api.add_resource(order_api.OrderListResource, '/api/orders')
+    api.add_resource(users_api.UserResource, '/api/users/<int:user_id>')
+    api.add_resource(users_api.UserListResource, '/api/users')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    db_session.global_init("db/database.sqlite")
+    api_connect()
+    app.run('127.0.0.1', port=8080)
